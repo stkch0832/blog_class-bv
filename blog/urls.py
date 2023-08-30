7254ddae956a8748996f134677a73ae1d19eea4d
stkch0832 <stkch0832@gmail.com>
2023-08-30 22:20:09 +0900

    Add: comment's CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('article/<int:pk>/edit/', views.ArticleEditView.as_view(), name='article_edit'),
     path('article/<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
 
+    path('article/<int:pk>/<int:comment_id>/delete/', views.CommentDelete.as_view(), name='comment_delete'),
+
     path('category/<str:category>/', views.CategoryView.as_view(), name='category'),
     path('search/', views.SearchView.as_view(), name='search'),
+
 ]
